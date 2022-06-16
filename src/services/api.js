4859fe930cbe2aedeb371024d63b94b0e0dddaf7
Rafael-PC <rafael_rx@hotmail.com>
2022-06-16 13:27:00 -0300

    deploy: api address

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const token = localStorage.getItem('token');
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://suri-wallet-api-k7z65.ondigitalocean.app/',
   headers: {'Authorization': `Bearer ${token}`, 'access': localStorage.getItem("rules").split(",")}
 });
 
