4ce7ced76a8dd2f6d64cfdfb4f23b08b7c7d2a6d
Rafael-PC <rafael_rx@hotmail.com>
2022-06-16 13:39:31 -0300

    fix: access header

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 const token = localStorage.getItem('token');
 
+let rules = localStorage.getItem("rules") || '';
+if(rules !== ''){
+  rules = rules.split(',');
+}
 const api = axios.create({
   baseURL: 'https://suri-wallet-api-k7z65.ondigitalocean.app/',
-  headers: {'Authorization': `Bearer ${token}`, 'access': localStorage.getItem("rules").split(",")}
+  headers: {'Authorization': `Bearer ${token}`, 'access': rules}
 });
 
 export default api;
